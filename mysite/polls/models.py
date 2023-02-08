1958aa92e25c9fc2cfec76112b375b47af82fdf0
Søren Hermansen <Soren.harbo.hermansen@gmail.com>
2023-02-08 15:12:27 +0100

    Updated a bunch of things

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,3 +19,18 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    
+    
+    
+#class Stock(models.Model):
+#    #ticker = models.CharField(max_length=64) same as symbol
+#    exchange = models.CharField(max_length=64)
+#    symbol = models.CharField(max_length=64)
+#    name = models.CharField(max_length=200)
+#    price = models.DecimalField()
+#    last_updated = models.DateTimeField('Last Updated')
+
+#class Stock_History(models.Models):
+#    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+#    opening_price = models.DecimalField(max_digits=200)
+#    closing_price = models.DecimalField(max_digits=200)
